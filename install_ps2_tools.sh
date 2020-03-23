7578f995a0b69f76c95ca5a742c06ec422b00e69
gymreklab <melissa.gymrek@gmail.com>
2020-03-23 16:43:17 -0700

    adding RFMix

--- install_ps2_tools.sh
+++ install_ps2_tools.sh
@@ -2,6 +2,15 @@
 
 set -e
 
+# Install RFMix
+cd /sources
+wget https://www.dropbox.com/s/cmq4saduh9gozi9/RFMix_v1.5.4.zip
+unzip RFMix_v1.5.4.zip
+cd RFMix_v1.5.4/PopPhased/
+g++ -Wall -O3 -ftree-vectorize -fopenmp main.cpp getdata.cpp randomforest.cpp crfviterbi.cpp windowtosnp.cpp -o RFMix_PopPhased
+cd ../TrioPhased
+g++ -Wall -O3 -ftree-vectorize -fopenmp main.cpp getdata.cpp randomforest.cpp crfviterbi.cpp windowtosnp.cpp -o RFMix_TrioPhased
+
 # Install IMPUTE2
 cd /sources
 wget https://mathgen.stats.ox.ac.uk/impute/impute_v2.3.2_x86_64_static.tgz
